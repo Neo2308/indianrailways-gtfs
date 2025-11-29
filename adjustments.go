ada7c170a4abc5b0717890fc4838b3ffcff468b3
Neo2308 <pradha.krishna.cse17@itbhu.ac.in>
2025-11-29 13:54:14 +0530

    Sort data before exporting to gtfs
    * Sort the data before exporting to gtfs

--- adjustments.go
+++ adjustments.go
@@ -32,10 +32,8 @@ func NewDataErrors() (*DataErrors, error) {
 var stationsFixingErrors = 0
 var stationsFixingWarnings = 0
 
-func fixStation(station *Station) {
-	if fix, ok := IncorrectStationCodes[station.Code]; ok {
 func fixStation(station *Station, fixes *DataErrors) {
-	if fix, ok := (fixes.Overrides)[station.Code]; ok {
+	if fix, ok := fixes.Overrides[station.Code]; ok {
 		station.Code = fix.Code
 		station.Name = fix.Name
 		station.Lat = fix.Lat
@@ -43,7 +41,7 @@ func fixStation(station *Station, fixes *DataErrors) {
 	}
 	if station.Lat == "" || station.Lng == "" {
 		// fmt.Println("Fixing station has no latitude and longitude: ", station.Code)
-		if fix, ok := (fixes.Updates)[station.Code]; ok {
+		if fix, ok := fixes.Updates[station.Code]; ok {
 			station.Lat = fix.Lat
 			station.Lng = fix.Lng
 			if strings.TrimSpace(station.Name) == "" {
@@ -60,7 +58,7 @@ func fixStation(station *Station, fixes *DataErrors) {
 			fmt.Println("!!! ERROR !!! No fix of position found for station: ", station.Code)
 		}
 	} else if strings.TrimSpace(station.Name) == "" {
-		if fix, ok := (fixes.Updates)[station.Code]; ok {
+		if fix, ok := fixes.Updates[station.Code]; ok {
 			station.Name = fix.Name
 			if strings.TrimSpace(fix.Name) == "" {
 				station.Name = station.Code
